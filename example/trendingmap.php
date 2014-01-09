d2bde9d638ba8f464cee78f9526768bd6e96f080
sonylnagale <sonyl@livefyre.com>
2014-01-09 00:09:09 -0500

    Think I got it

--- example/trendingmap.php
+++ example/trendingmap.php
@@ -1,3 +1,7 @@
+<?php
+	$article = $_REQUEST['subject'];
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -15,12 +19,27 @@
 <body>
 	<div id="lf_map"></div>
 <script>
+var collections = {
+		'snow': { 
+			siteId: '333682',
+			articleId: 'sn-1389203378710'
+		},
+		'cold': {
+			siteId: '333682', 
+	        articleId: 'sn-1389203417469'
+		},
+		'winter': {
+			siteId: '333682', 
+		    articleId: 'sn-1389203732013'
+		}
+}
+
 
 Livefyre.require(['streamhub-sdk/collection','streamhub-map/content/content-map-view'], function(Collection,ContentMapView){
     var collection = new Collection({
         network: "client-solutions.fyre.co",
         siteId: 333682,
-        articleId: "sn-1389203417469"
+        articleId: collections.<?php echo $article; ?>.articleId
     });
     var view = new ContentMapView({
         el: document.getElementById("lf_map"),
